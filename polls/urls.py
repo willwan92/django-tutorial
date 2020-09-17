53bd75f9c3fa1219f8381c0671c61bb701662459
willwan92 <wilson3cy@163.com>
2020-09-17 10:29:03 +0800

    使用通用视图重构投票应用

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:question_id>/', views.detail, name='detail'),
-	path('<int:question_id>/results/', views.results, name='results'),
-	path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    # DetailView 期望从 URL 中捕获名为 "pk" 的主键值，所以我们为通用视图把 question_id 改成 pk 。
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
